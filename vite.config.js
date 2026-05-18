0034defcf66545c2c5cf03148011c37ce2560ad5
Daniel Adeleye <136744803+Tireni4560@users.noreply.github.com>
2026-05-18 01:30:45 -0700

    Update Vite build output directory to 'dist'

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'public', // tells Vite to start from /public
-  build: {
-    outDir: '../dist', // output goes one level up, into /dist
-  },
   plugins: [react()],
   server: {
     port: 5173,
   },
+  build: {
+    outDir: 'dist',
+  }
 })
